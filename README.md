b67de3064363503276adbd6eee955c41dd4635d1
Aldjinn <aldjinn@gmail.com>
2021-03-01 20:06:15 +0100

    expose metrics

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Used libraries:
 - webserver: [express](https://github.com/expressjs/express)
 - http client: [axios](https://github.com/axios/axios)
 - env variable handling: [dotenv-defaults](https://github.com/mrsteele/dotenv-defaults)
+- prometheus metrics: [prometheus-api-metrics](https://github.com/PayU/prometheus-api-metrics)
+- prometheus client: [prom-client](https://github.com/siimon/prom-client)
 
 ## Development
 
@@ -19,7 +21,7 @@ npm install
 node server.js
 ```
 
-Now open http://localhost:13080/ in your browser.
+Now open http://localhost:13080/status in your browser.
 
 ## Docker
 
@@ -38,14 +40,29 @@ docker run -e VALHEIM_HOST='my.valheim.host' \
     -e TELEGRAM_CHAT_ID='*-123456789' \
     -e TELEGRAM_BOT='bot123456789:nuG0iuy7ae9eVah5eef8tahXee6eij8nieD' \
     -e TELEGRAM_ENABLED='false' \
+    -e METRICS_ENABLED='true' \
     -p 13080:13080 \
     aldjinn/valheim-server-status:latest
 ```
 
-Now open http://localhost:13080/ in your browser again.
+Now open http://localhost:13080/status in your browser again.
 
 ## Telegram
 
 If you have configured the Telegram settings correctly, a similar message should be sent to your chat after the application has started. As well if someone joines or leaves the server.
 
 ![telegram.png)](telegram.png)
+
+## Prometheus
+
+Some metrics are available for scraping via http://localhost:13080/metrics/ if the application is started with METRICS_ENABLED='true'.
+
+```
+# HELP player_count number of players
+# TYPE player_count gauge
+player_count 1
+
+# HELP server_info server info
+# TYPE server_info gauge
+server_info{version="0.146.11",name="TheBigBadWolf",map="TheBigBadWolf"} 1
+```
--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const express = require("express");
 const schedule = require("node-schedule");
 const gamedig = require("gamedig");
 const axios = require("axios");
+const apiMetrics = require("prometheus-api-metrics");
+const prometheus = require("prom-client");
+
+const playerCount = new prometheus.Gauge({
+  name: "player_count",
+  help: "number of players",
+  labelNames: ["player_count"],
+});
+
+const serverInfo = new prometheus.Gauge({
+  name: "server_info",
+  help: "server info",
+  labelNames: ["version", "name", "map"],
+});
 
 const PORT = 13080;
 const HOST = "0.0.0.0";
@@ -14,9 +28,14 @@ let gamedigResult = "No query yet!";
 let currentNumberOfPlayers = -1;
 
 const app = express();
-app.get("/", (req, res) => {
+
+app.get("/status", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify(gamedigResult, null, "\t"));
+});
+
+app.get("/", (req, res) => {
+  res.send("valheim-server-status");
 });
 
 const job = schedule.scheduleJob(process.env.VALHEIM_QUERY_CRON, function () {
@@ -40,11 +59,26 @@ function queryServer() {
       gamedigResult = json;
 
       console.log(gamedigResult);
+      adjustMetrics();
       checkPlayerLeftOrJoined();
     })
     .catch((error) => {
       console.log("Server is offline: " + error);
     });
+}
+
+function adjustMetrics() {
+  if (process.env.METRICS_ENABLED === "true") {
+    playerCount.set(gamedigResult.players.length);
+    serverInfo.set(
+      {
+        version: gamedigResult.raw.tags,
+        name: gamedigResult.name,
+        map: gamedigResult.map,
+      },
+      1
+    );
+  }
 }
 
 function checkPlayerLeftOrJoined() {
@@ -107,7 +141,11 @@ function main() {
   console.log(`TELEGRAM_CHAT_ID=${process.env.TELEGRAM_CHAT_ID}`);
   console.log(`TELEGRAM_BOT=${process.env.TELEGRAM_BOT}`);
   console.log(`TELEGRAM_ENABLED=${process.env.TELEGRAM_ENABLED}`);
+  console.log(`METRICS_ENABLED=${process.env.METRICS_ENABLED}`);
 
+  if (process.env.METRICS_ENABLED === "true") {
+    app.use(apiMetrics());
+  }
   app.listen(PORT, HOST);
   const startMessage = `Valheim Server Status running on http://${HOST}:${PORT}`;
   console.log(startMessage);
